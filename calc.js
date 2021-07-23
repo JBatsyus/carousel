a08f843e78ba902c26a5de860032d7d5c91f1dc1
JBatsyus <jkalabina@gmail.com>
2021-07-23 21:28:50 +0200

    Добавление калькулятора (практика)

---
+++ calc.js
@@ -0,0 +1,43 @@
+function plus() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 + num2;
+    document.getElementById('out').value = result;
+}
+function subtraction() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 - num2;
+    document.getElementById('out').value = result;
+
+}
+function multiplication() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 * num2;
+    document.getElementById('out').value = result;
+
+}
+function division () {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+    result = num1 / num2;
+    document.getElementById('out').value = result;
+
+}
+
+function myFunction() {
+    document.getElementById("myForm").reset();
+}
